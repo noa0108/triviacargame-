5be19ee129ad061ad41efb7ec8307c9c170bb727
Neddam <0564873@students.dadeschools.net>
2019-04-16 14:08:40 -0400

    Finish html , started js

---
+++ main.js
@@ -0,0 +1,23 @@
+var panel = $("quiz-area");
+
+var countStartNumber = 30;
+
+// Question set 
+var Questions = [
+   
+    {
+        Question: "What is the date of World War 1?" , 
+        Answers: ["1914-1918", "1917-192020" , "1939-1945"],
+        correctAnswer: "1914-1918",
+        image: "trivia gif.jfif" 
+     },
+
+     {
+        Question: "which of these is Not a name of a planet ?" , 
+        Answers: ["Pluto", "Mars" , "Earth", "Vanus"],
+        correctAnswer: "Vanus",
+        image: "assets\trivia planets.jfif" 
+     }
+
+
+]
