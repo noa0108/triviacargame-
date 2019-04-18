4dfeb955f707c72b291ba719e304e3751d4839df
Neddam <0564873@students.dadeschools.net>
2019-04-18 14:09:54 -0400

    Almost done

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var Questions = [
      },
 
      {
-        Question: "which of these is Not a name of a planet ?" , 
+        Question: "which of these is NOT a name of a planet ?" , 
         Answers: ["Pluto", "Mars" , "Earth", "Vanus"],
         correctAnswer: "Vanus",
         image: "assets\trivia planets.jfif" 
@@ -21,3 +21,48 @@ var Questions = [
 
 
 ]
+
+//Variable to hold our setInterval 
+var timer;
+
+var game = {
+
+
+   questions: questions,
+   currentQuestion: 0,
+   counter: countStartNumber,
+   correct: 0,
+   incorrect: 0,
+
+
+   countdown: function() {
+      game.counter--
+      $("#counter-number").html(game.counter);
+      if (game-counter === 0) {
+         console.log("TIME UP");
+         game.timeUp();
+
+      }
+   },
+
+   loadQuestion: function() {
+      timer = setInterval(game.countdown, 1000);
+      panel.html("<h2>"+ questions [this.currentQuestion].question + "</h2>");
+      for (var i=0; i < questions[this.currentQuestion].answers.length; i++ )
+      {
+         panel.append("<button class='answer-button' id='button' data-name=' " + 
+         questions[this.currentQuestion].answer[i]+
+         "'>" + question[this.currentQuestion].answers[i] + "</button>");
+      }
+   },
+
+   nextQuestion: function() {
+      game.counter = countStartNumber;
+      $("#counter-number").html(game.counter);
+      game.currentQuestion++;
+      game.loadQuestion();
+   },
+
+   timeUp
+}
+
